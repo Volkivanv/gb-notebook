88723e3aa912d0a5a0722bce72dd99a1ae062d49
Ivan Volkov <volkivanv@mail.ru>
2024-02-14 08:12:57 +0300

    Перед выполнением 5ДЗ

---
+++ src/notebook/util/UserValidator.java
@@ -0,0 +1,23 @@
+package notebook.util;
+
+import notebook.model.User;
+
+public class UserValidator {
+    public User validate(User user){
+        if(!isValid(user)){
+            throw new IllegalArgumentException("Введены не корректные данные");
+        }
+//        if(user.getFirstName().isEmpty()) throw new IllegalArgumentException("Имя не должно быть пустым");
+//        if(user.getLastName().isEmpty()) throw new IllegalArgumentException("Фамилия не должна быть пустой");
+//        if(user.getPhone().isEmpty()) throw new IllegalArgumentException("Телефонный номер не должен быть пустым");
+        user.setFirstName(user.getFirstName().replaceAll(" ","").trim());
+        user.setLastName(user.getLastName().replaceAll(" ","").trim());
+        user.setPhone(user.getPhone().replaceAll(" ","").trim());
+        return user;
+    }
+    private boolean isValid(User user){
+        return !user.getFirstName().isEmpty() &&
+                user.getLastName().isEmpty() &&
+                user.getPhone().isEmpty();
+    }
+}
